6d4031a5da9cf160e5bb7a590e57d066c1900435
camilo andres lemos <camilolemus30@gmail.com>
2023-10-18 22:30:17 -0500

    crear campos del formulario por el modelo- funcion eliminar

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import libro
-
+from .forms import LibroForm
 
 # Create your views here.
 
@@ -19,8 +19,18 @@ def listar(request):
     return render(request, "libros/index.html", {'libros': libros})
 
 def crear(request):
-    return render(request, "libros/crear.html")
+    formulario =LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('listar')
+    return render(request, "libros/crear.html", {'formulario': formulario})
+
 
 def editar(request):
     return render(request, "libros/editar.html")
 
+
+def eliminar(request, id):
+    Libro = libro.objects.get(id=id)
+    Libro.delete()
+    return redirect('listar')
